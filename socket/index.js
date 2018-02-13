773b458d442882cbfc4924b77c5d1dd915360bd2
rajanrai93 <rajanrai93@icloud.com>
2018-02-13 00:25:52 -0800

    commited

---
+++ socket/index.js
@@ -0,0 +1,54 @@
+const server = require("http").Server();
+const port = process.env.PORT || 10001;
+
+var io = require("socket.io")(server);
+
+var usernames = [];
+var msgs = [];
+
+
+io.on("connection", function(socket){
+    console.log("user is connected");
+    
+    
+    socket.on("username", function(data){
+        
+        console.log("User is giving username: "+data);
+        usernames.push(data);
+        
+        io.emit("usersjoined", usernames);
+        
+        
+    })
+    
+    socket.on("sendChat", function(data){
+        console.log("users sent a msg");
+        msgs.push(data);
+        
+        io.emit("msgsent", msgs);
+        
+        
+        
+    });
+    
+    socket.on("disconnect", function(){
+        console.log("user has disconnected");
+        
+        
+        
+    })
+    
+});
+
+server.listen(port, (err)=>{
+    if(err){
+        console.log("Error: "+err);
+        return false;
+        
+        
+        
+    }
+    console.log("Socket port is running");
+    
+    
+});
