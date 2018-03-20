0fe580bab81980e6df338e2cdbae0adf9000cd27
rajanrai93 <rajanrai93@icloud.com>
2018-03-20 00:48:00 -0700

    added change color func

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const server = require("http").Server();
 const port = process.env.PORT || 10001;
+const server = require("http").Server();
 
 var io = require("socket.io")(server);
 
+var allusers = {};
+var allstickers = {};
 var usernames = [];
 var msgs = [];
-
-server.listen(port, (err)=> {
-    if(err) {
-        console.log("it is Working " + err);
-        return false;
-    }
-})
-
 
 
 io.on("connection", function(socket){
@@ -39,6 +33,13 @@ io.on("connection", function(socket){
         
     });
     
+      socket.on('change color', (color) => {
+    // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
+    // we make use of the socket.emit method again with the argument given to use from the callback function above
+    console.log('Color Changed to: ', color)
+    io.sockets.emit('change color', color)
+  })
+    
     socket.on("disconnect", function(){
         console.log("user has disconnected");
         
@@ -48,15 +49,59 @@ io.on("connection", function(socket){
     
 });
 
+io.on("connection", function(socket){
+    console.log("user is connected");
+	
+    socket.on("stick", function(data){
+        allstickers[this.myRoom].push(data);
+        
+        io.to(this.myRoom).emit("newsticker", allstickers[this.myRoom]);
+    });
+    
+    
+    
+    
+    
+    socket.on("joinroom", function(data){
+        console.log(data);
+        socket.join(data);
+        
+        socket.myRoom = data;
+        socket.emit("yourid", socket.id);
+        
+        if(!allusers[data]){
+            allusers[data] = [];
+        }
+        
+        if(!allstickers[data]){
+            allstickers[data] = [];
+        }
+        
+        allusers[data].push(socket.id);
+        io.to(data).emit("userjoined", allusers[data]);
+        io.to(data).emit("newsticker", allstickers[data]);
+        
+    });
+    
+    socket.on("mymove", function(data){
+        console.log(data);
+        socket.to(this.myRoom).emit("newmove", data); 
+    });
+    
+    socket.on("disconnect", function(){
+        if(this.myRoom){
+            var index = allusers[this.myRoom].indexOf(socket.id);
+            allusers[this.myRoom].splice(index,1);
+            io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);
+        }
+    })
+});
+
 server.listen(port, (err)=>{
     if(err){
-        console.log("Error: "+err);
+        console.log(err);
         return false;
-        
-        
-        
     }
-    console.log("Socket port is running");
     
-    
-});
+    console.log("Port is running");
+})
