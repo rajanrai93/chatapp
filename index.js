fccc48462a59a94b6193ce76df0ae332b77e8303
rajanrai93 <rajanrai93@icloud.com>
2018-03-21 00:48:44 -0700

    chaged server codes

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var allusers = {};
 var allstickers = {};
 var usernames = [];
 var msgs = [];
-var allqs = {};
 
 
 io.on("connection", function(socket){
@@ -60,52 +59,6 @@ io.on("connection", function(socket){
     
     /////
     
-
-
-
-io.on("connection", function(socket){
-    
-    socket.on("joinroom", function(data){
-        console.log("joining room", data);
-        
-        socket.join(data);
-        socket.myRoom = data;
-        
-        if(!allqs[data]){
-            allqs[data] = {
-                qobj:{}
-            };   
-        }
-        
-    });
-    
-    socket.on("answer", function(data){
-        var msg = "WRONG!";
-        if(data == allqs[socket.myRoom].qobj.a){
-            msg = "You've won in life";
-        }
-        socket.emit("result", msg);
-    });
-    
-    socket.on("qsubmit", function(data){
-        allqs[socket.myRoom].qobj = data;
-        socket.to(socket.myRoom).emit("newq", data); 
-    });
-    
-    socket.on("disconnect", function(){
-        
-    })
-});
-
-server.listen(port, (err)=>{
-    if(err){
-        console.log(err);
-        return false;
-    }
-    
-    console.log("Port is running");
-})
-    /////
     
     
     
