ce23ef58c4ffe0622d2aa931f6400055bfbb3b8d
rajanrai93 <rajanrai93@icloud.com>
2018-03-23 18:23:37 -0700

    changed server ports

--- index.js
+++ index.js
@@ -58,6 +58,51 @@ io.on("connection", function(socket){
     });
     
     /////
+    var allqs = {};
+
+io.on("connection", function(socket){
+    
+    socket.on("joinroom", function(data){
+        console.log("joining room", data);
+        
+        socket.join(data);
+        socket.myRoom = data;
+        
+        if(!allqs[data]){
+            allqs[data] = {
+                qobj:{}
+            };   
+        }
+        
+    });
+    
+    socket.on("answer", function(data){
+        var msg = "WRONG!";
+        if(data == allqs[socket.myRoom].qobj.a){
+            msg = "You've won in life";
+        }
+        socket.emit("result", msg);
+    });
+    
+    socket.on("qsubmit", function(data){
+        allqs[socket.myRoom].qobj = data;
+        socket.to(socket.myRoom).emit("newq", data); 
+    });
+    
+    socket.on("disconnect", function(){
+        
+    })
+});
+
+server.listen(port, (err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    
+    console.log("Port is running");
+})
+    /////
     
     
     
